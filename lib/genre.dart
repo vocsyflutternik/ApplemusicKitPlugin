af1a0a5517d552e9a8f1e2e48fb5b8375f3e84e2
nikhil <vocsy.nikhil@gmail.com>
2021-08-23 11:38:51 +0530

    initial commit

---
+++ lib/genre.dart
@@ -0,0 +1,18 @@
+part of applemusickitpackage;
+
+class Genre {
+  final String id;
+  final String type;
+  final String href;
+  final String name;
+
+  Genre({this.id, this.type, this.href, this.name});
+
+  factory Genre.fromJson(Map<String, dynamic> json) {
+    return Genre(
+        id: json['id'],
+        type: json['type'],
+        href: json['href'],
+        name: json['attributes']['name']);
+  }
+}
